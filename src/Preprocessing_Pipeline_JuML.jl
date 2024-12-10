e940af8250c0865b22365b4e662661c1ace39c29
iznarf <franzidonhauser@googlemail.com>
2024-12-10 13:57:38 +0100

    first steps

--- src/Preprocessing_Pipeline_JuML.jl
+++ src/Preprocessing_Pipeline_JuML.jl
@@ -1,7 +1,9 @@
 module Preprocessing_Pipeline_JuML
-
+include("punctuation_removal.jl")
 timesx(x, y) = y*x
 
 export timesx
+export clean_text
+export clean_tokens
 
 end
--- src/punctuation_removal.jl
+++ src/punctuation_removal.jl
@@ -1,8 +1,3 @@
-### A Pluto.jl notebook ###
-# v0.20.0
-
-using Markdown
-using InteractiveUtils
 
 # ╔═╡ 80f24360-b60f-11ef-1017-652e59097489
 # Eingabe ist eine Liste von zusammenhängenden Strings (Sätzen)
@@ -11,20 +6,6 @@ function clean_text(text::AbstractString)::AbstractString
     return replace(text, r"[[:punct:](){}[\]\"']" => "")
 end
 
-
-# ╔═╡ efa5f071-ea3f-4cba-b336-5140442cd633
-# Beispieleingabe
-# was machen wir  mit den Zeichen "" innerhalb eines Strings?
-texts = [
-    "Giga{{{}}}, 'chad'!?!", 
-    "Kenning West is a \"test\".", 
-    "Let... him {cook}!! []?"
-]
-
-# ╔═╡ c83dad14-fb41-41c1-b80d-4ff54067f832
-# Test 
-cleaned_text = clean_text.(texts)
-
 # ╔═╡ 9710b080-ea41-42ff-8b91-50c88ea94206
 # Eingabe ist Liste bestehend aus Tokens
 function clean_tokens(tokens::Vector{String})::Vector{String}
@@ -32,13 +13,9 @@ function clean_tokens(tokens::Vector{String})::Vector{String}
     return clean_token.(tokens)
 end
 
-# ╔═╡ 7b9e9e84-377a-44f0-8818-10a0317e83cb
-# Beispieleingabe
-tokens = ["Hello,", "'world'!", "This", "is", "a", "\"test\".", "Does", "it", "remove", "{all}", "[braces]", "and", "punctuation?"]
+export clean_text
+export clean_tokens
 
-# ╔═╡ 22f3c1fd-37e6-418d-a6f0-a5f8ee5f92fd
-# Test
-cleaned_tokens = clean_tokens(tokens)
 
 # ╔═╡ Cell order:
 # ╠═80f24360-b60f-11ef-1017-652e59097489

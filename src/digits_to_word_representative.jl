69eb2c62809b8c57c09a0f3b0dd56c1f01bed585
marike-v <marike_vossbeck@posteo.de>
2024-12-16 17:14:10 +0100

    changes digits to their word representative

---
+++ src/digits_to_word_representative.jl
@@ -0,0 +1,21 @@
+#changing numbers in numerical format to string representation in english,
+# allowed inputs Strings and Vector{String}
+function num_to_string(input)
+    digit_to_word = Dict(
+        "0" => "zero", "1" => "one", "2" => "two", "3" => "three", 
+        "4" => "four", "5" => "five", "6" => "six", "7" => "seven", 
+        "8" => "eight", "9" => "nine"
+    )
+    if typeof(input)==String
+        str_wo_num= replace(input, r"[0-9]"=>digit->digit_to_word[digit]) 
+        
+    elseif typeof(input)==Vector{String}        #replacing for each element in list
+        str_wo_num=[]
+        for i in input
+            push!(str_wo_num,num_to_string(i))
+        end 
+              
+    end
+    return str_wo_num
+end
+export num_to_string

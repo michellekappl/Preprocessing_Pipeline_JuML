377f325eed961a779985fa2a3367e0755ef27451
michellekappl <michelle.kappl@gmail.com>
2024-12-16 17:30:21 +0100

    change one_hot encoding to work with nlp pipeline

--- src/one_hot_encoding.jl
+++ src/one_hot_encoding.jl
@@ -1,57 +1,26 @@
-function one_hot(data, vocab = [])
+function one_hot(pipe :: TokenizedNlpPipe)
     """
     one_hot(x)
 
-    Create a one-hot-encoding out of given vocabulary
+    Create a one-hot-encoding out of given TokenizedNlpPipe
 
-    # Examples
-    ```jldoctest
-    julia> a = [["hello", "world"], ["second", "sentence"]]
-    julia> one_hot(a)
-    2×4 Array{Int64}:
-     1  1  0  0
-     0  0  1  1
-    ```
     """
-    if length(vocab) == 0
-        vocab = get_vocab(data)
-    end
-    vocab_dict = get_vocab_dict(vocab)
-    one_hot_data = zeros(Int, length(data), length(vocab))
-    for (i, sentence) in enumerate(data)
+
+    vocab_dict = get_vocab_dict(pipe.vocabulary)
+    tokens = []
+    length_vocab = length(pipe.vocabulary)
+
+    for (i, sentence) in enumerate(pipe.data)
+        sentence_tokens = []
         for word in sentence
-            one_hot_data[i, vocab_dict[word]] = 1
+            word_vector = zeros(length_vocab)
+            word_vector[vocab_dict[word]] = 1
+            push!(sentence_tokens, word_vector)
         end
+        push!(tokens, sentence_tokens)
     end
-    return one_hot_data
-end
 
-function get_vocab(data)
-    """
-    get_vocab(data)
-
-    Get the vocabulary from the data
-
-    # Examples
-    ```jldoctest
-    julia> a = [["hello", "world"], ["second", "sentence"]]
-    julia> get_vocab(a)
-    4-element Array{String}:
-     "hello"
-     "world"
-     "second"
-     "sentence"
-    ```
-    """
-    vocab = []
-    for sentence in data
-        for word in sentence
-            if word ∉ vocab
-                push!(vocab, word)
-            end
-        end
-    end
-    return vocab
+    return VectorizedNlpPipe(tokens=tokens, vocabulary=vocab_dict)
 end
 
 function get_vocab_dict(vocab)
@@ -60,13 +29,8 @@ function get_vocab_dict(vocab)
 
     Create a Dict with idx in vector as values and words as keys
 
-    # Examples
-    ```jldoctest
-    julia> a = ["hello", "world", "second", "sentence"]
-    julia> get_one_hot_for_vocab(a)
-    Dict("hello" => 1, "world" => 2, "second" => 3, "sentence" => 4)
-    ```
     """
+
     vocab_dict = Dict()
     for (i, word) in enumerate(vocab)
         vocab_dict[word] = i

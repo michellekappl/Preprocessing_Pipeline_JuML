7c7b679198da8e971e3d454354e636fcf52d839f
Leon Schreiber <leon.schreiber@tu-berlin.de>
2024-12-17 12:05:46 +0100

    Add unit tests for one noise removal

--- src/Preprocessing_Pipeline_JuML.jl
+++ src/Preprocessing_Pipeline_JuML.jl
@@ -10,16 +10,4 @@ include("tokenization/tokenizer.jl")
 include("preprocessing/removeNoise.jl")
 
 include("vectorization/oneHotEncoding.jl")
-
-test_corpus = [
-   "Hello <b>world</b>! Visit http://example.com.",
-   "Email me: test@example.com or call +123-456-7890.",
-   "Today is 12/25/2024, time now: 10:30AM.",
-   "My file is at C:\\Users\\JohnDoe\\Documents\\file.txt.",
-   "Check this out: www.awesome-website.org/about-us.html!",
-   "#JuliaLang is great. Follow us @JuliaNLP."
-]
-
-pipe = NlpPipe(test_corpus) |> remove_noise |> tokenize |> one_hot_encoding
-
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,29 @@
 using Preprocessing_Pipeline_JuML
 using Test
 
+test_corpus = [
+    "Hello <b>world</b>! Visit http://example.com.",
+    "Email me: test@example.com or call +123-456-7890.",
+    "Today is 12/25/2024, time now: 10:30AM.",
+    "My file is at C:\\Users\\JohnDoe\\Documents\\file.txt.",
+    "Check this out: www.awesome-website.org/about-us.html!",
+    "#JuliaLang is great. Follow us."
+]
+
 @testset "Preprocessing_Pipeline_JuML.jl" begin
 
-    @testset "timesx" begin
-        @test timesx(2, 3) == 6
-        @test timesx(3, 4) == 12
-        @test timesx(0, 9) == 0
+    @testset "remove_noise" begin
+        pipe = NlpPipe(test_corpus) |> remove_noise
+
+        pipe.corpus |> println
+
+        @test pipe.corpus[1] == "Hello world! Visit "
+        @test pipe.corpus[2] == "Email me  or call 1234567890."
+        @test pipe.corpus[3] == "Today is 12 time now ."
+        @test pipe.corpus[4] == "My file is at "
+        @test pipe.corpus[5] == "Check this out "
+        @test pipe.corpus[6] == " is great. Follow us."
     end
 
+    # TODO Add more tests
 end

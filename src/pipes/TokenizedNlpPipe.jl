c3556c9dca47eff15129b1b73e587b012613161e
Leon Schreiber <leon.schreiber@tu-berlin.de>
2024-12-17 11:28:15 +0100

    - Reorganise and rename files - Add `VectorizedNlpPipe` - Fit existing preprocessing functions into NlpPipe schema - Add more Regexes to noise removal preprocessing step

--- src/pipes/TokenizedNlpPipe.jl
+++ src/pipes/TokenizedNlpPipe.jl
@@ -3,11 +3,13 @@ struct TokenizedNlpPipe
    tokens::Vector{Vector{String}}
    vocabulary::Set{String}
 
+   # Constructor for the first pipe in the pipeline
    function TokenizedNlpPipe(corpus::Vector{String}, tokens::Vector{Vector{String}})
       vocabulary = Set{String}(reduce(vcat, tokens))
       new(corpus, tokens, vocabulary)
    end
 
+   # Constructor for subsequent pipes in the pipeline (tokens and vocab are copied by default)
    function TokenizedNlpPipe(previousPipe::TokenizedNlpPipe; tokens::Vector{Vector{String}} = previousPipe.tokens, vocabulary::Set{String} = previousPipe.vocabulary)
       new(previousPipe.corpus, tokens, vocabulary)
    end

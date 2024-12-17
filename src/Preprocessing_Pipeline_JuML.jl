126fe4dd877dffa31b996de5607512627af36bd9
Leon Schreiber <leon.schreiber@tu-berlin.de>
2024-12-17 11:28:15 +0100

    - Reorganise and rename files - Add `VectorizedNlpPipe` - Fit existing preprocessing functions into NlpPipe schema - Add more Regexes to noise removal preprocessing step

--- src/Preprocessing_Pipeline_JuML.jl
+++ src/Preprocessing_Pipeline_JuML.jl
@@ -1,12 +1,25 @@
 module Preprocessing_Pipeline_JuML
 
-include("playground.jl")
+include("pipes/NlpPipe.jl")
+include("pipes/TokenizedNlpPipe.jl")
+include("pipes/VectorizedNlpPipe.jl")
 
-timesx(x, y) = y*x
+include("tokenization/tokenizeLevel.jl")
+include("tokenization/tokenizer.jl")
 
-include("cleaning_text.jl")
-include("one_hot_encoding.jl")
+include("preprocessing/removeNoise.jl")
 
-export timesx, one_got_encoding, cleaning_text
+include("vectorization/oneHotEncoding.jl")
+
+test_corpus = [
+   "Hello <b>world</b>! Visit http://example.com.",
+   "Email me: test@example.com or call +123-456-7890.",
+   "Today is 12/25/2024, time now: 10:30AM.",
+   "My file is at C:\\Users\\JohnDoe\\Documents\\file.txt.",
+   "Check this out: www.awesome-website.org/about-us.html!",
+   "#JuliaLang is great. Follow us @JuliaNLP."
+]
+
+pipe = NlpPipe(test_corpus) |> remove_noise |> tokenize |> one_hot_encoding
 
 end

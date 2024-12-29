c0c0e89b8588b33c5de131352cd4113dcc55ed42
Marike Vossbeck <marike_vossbeck@posteo.de>
2024-12-29 11:29:26 +0100

    replaces any number with word "number"

---
+++ src/preprocessing/nrs_to_nr-word.jl
@@ -0,0 +1,18 @@
+"""Allows user to preprocess numbers within a string. Each number, independent of length gets replaced with word: *number*, 
+inputs: Strings and Vector{String}
+output: Strings and Vector{String} respectively
+"""
+
+function number_to_word(input)
+    if typeof(input)==String
+       nums_to_word = replace(input, r"\d+" => "number") 
+        
+    elseif typeof(input)==Vector{String}    
+        nums_to_word=[]
+        for i in input
+            push!(nums_to_word,number_to_word(i))
+        end 
+    end
+    return nums_to_word
+
+end

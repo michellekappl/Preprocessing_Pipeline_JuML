4033d63fdf5e9c63b82876bb45d4b08f3b452e89
Marike Vossbeck <marike_vossbeck@posteo.de>
2024-12-29 17:43:35 +0100

    used package not yet correctly included

--- src/tokenization/stopWordRemoval.jl
+++ src/tokenization/stopWordRemoval.jl
@@ -1,9 +1,9 @@
 """
 stop_word_removal(input)
 
-removes predefined stopwords, 
+removes predefined stopwords, default is english, others can be choosen easily
 """
-
+#
 #hab leider keine ahnung merh wie man das mit den packages die man nutzt macht -.-, https://github.com/guo-yong-zhi/StopWords.jl/blob/main/README.md
 function stop_word_removal(input::Vector{String}, language="English")
     @eval using StopWords

cab3f31bc9436c8ef29ca92b971b2bdb47bef75d
Marike Vossbeck <marike_vossbeck@posteo.de>
2025-01-16 10:26:09 +0100

    custom stopword list can now be provided.

--- src/tokenization/stopWordRemoval.jl
+++ src/tokenization/stopWordRemoval.jl
@@ -5,17 +5,17 @@ removes predefined stopwords, default is english, others can be choosen easily
 """
 #
 #hab leider keine ahnung merh wie man das mit den packages die man nutzt macht -.-, https://github.com/guo-yong-zhi/StopWords.jl/blob/main/README.md
-function stop_word_removal(input::Vector{String}, language="English")
+function stop_word_removal(input::Vector{String}; language::String="English", custom_stopwords::Union{Nothing, Set{String}}=nothing)
     @eval using StopWords
-    stopwords_set=stopwords[language]
+    #use default stopword list or use customer provided one
+    stopwords_set = custom_stopwords === nothing ? stopwords[language] : custom_stopwords
+    
     for i in (length(input):-1:1)
         if input[i] in stopwords_set
-            deleteat!(input,i)
-
+            deleteat!(input, i)
         end
-
     end
     return input
-
 end
+
 export stop_word_removal

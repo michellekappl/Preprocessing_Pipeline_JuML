e83ed5a35faca89371cf3b07872f1a6d65c9188d
michellekappl <michelle.kappl@gmail.com>
2025-01-20 17:49:18 +0100

    final stemming implemented

--- src/tokenization/stemming.jl
+++ src/tokenization/stemming.jl
@@ -1,17 +1,21 @@
+using SnowballStemmer
+
 """
 stemming(input) stems words to their origin, default language is english, we're using SnowballStemmer.jl
 """
-
-
-function stemming(input::Vector{String}; language::String="english")
-    @eval using SnowballStemmer
+function stemming(pipe::TokenizedNlpPipe; language::String="english")::TokenizedNlpPipe
     #create stemmer specified language
     stemmer = Stemmer(language)
     
     #do stemming to each word from input
-    stemmed_words = [stem(stemmer, word) for word in input]
+    #stemmed_words = [stem(stemmer, word) for word in input]
+    stemmed_data = Vector{Vector{String}}()
+    for doc in pipe.tokens
+        stemmed_doc = [stem(stemmer, word) for word in doc]
+        push!(stemmed_data, stemmed_doc)
+    end
     
-    return stemmed_words
+    return TokenizedNlpPipe(pipe.corpus, stemmed_data, pipe.labels)
 end
 
 export stemming

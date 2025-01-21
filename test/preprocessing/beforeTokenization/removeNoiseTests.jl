8252401086af231a3141e83561e057895cdc7fbd
Leon Schreiber <leon.schreiber@tu-berlin.de>
2025-01-21 17:40:45 +0100

    Change remove_noise so it masks instead of removing completely.

--- test/preprocessing/beforeTokenization/removeNoiseTests.jl
+++ test/preprocessing/beforeTokenization/removeNoiseTests.jl
@@ -1,19 +1,43 @@
+test_corpus = [
+   "Hello <b>world</b>! Visit http://example.com.",
+   "Email me: test@example.com or call +123-456-7890.",
+   "Today is 12/25/2024, time now: 10:30AM.",
+   "My file is at C:\\Users\\JohnDoe\\Documents\\file.txt.",
+   "Check this out: www.awesome-website.org/about-us.html!",
+   "#JuliaLang is great. Follow us."
+]
+
 @testset "Remove Noise Tests" begin
-   test_corpus = [
+   # ----------------------------------------------------------------
+   # Test with default replacement patterns
+   # ----------------------------------------------------------------
+   pipe = NlpPipe(test_corpus) |> remove_noise
+
+   @test pipe.corpus == [
+      "Hello world Visit <URL>",
+      "Email me <EMAIL> or call 1234567890",
+      "Today is <DATE> time now <TIME>",
+      "My file is at <PATH>",
+      "Check this out <URL>",
+      " is great Follow us"
+   ]
+
+   custom_replacement_patterns = [
+      r"JuliaLang" => "JUL1@",
+      r"My file is at" => "This is a Julia test",
+   ]
+
+   # ----------------------------------------------------------------
+   # Test with custom replacement patterns
+   # ----------------------------------------------------------------
+   pipe = NlpPipe(test_corpus) |> pipe -> remove_noise(pipe; replacement_patterns = custom_replacement_patterns)
+
+   @test pipe.corpus == [
       "Hello <b>world</b>! Visit http://example.com.",
       "Email me: test@example.com or call +123-456-7890.",
       "Today is 12/25/2024, time now: 10:30AM.",
-      "My file is at C:\\Users\\JohnDoe\\Documents\\file.txt.",
+      "This is a Julia test C:\\Users\\JohnDoe\\Documents\\file.txt.",
       "Check this out: www.awesome-website.org/about-us.html!",
-      "#JuliaLang is great. Follow us."
+      "#JUL1@ is great. Follow us."
    ]
-
-   pipe = NlpPipe(test_corpus) |> remove_noise
-
-   @test pipe.corpus[1] == "Hello world Visit "
-   @test pipe.corpus[2] == "Email me  or call 1234567890"
-   @test pipe.corpus[3] == "Today is 12 time now "
-   @test pipe.corpus[4] == "My file is at "
-   @test pipe.corpus[5] == "Check this out "
-   @test pipe.corpus[6] == " is great Follow us"
 end

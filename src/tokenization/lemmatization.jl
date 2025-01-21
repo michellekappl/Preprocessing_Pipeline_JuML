eb08a5c2bcbde6db7ec613ca935e6789e2cfb69a
Marike Vossbeck <marike_vossbeck@posteo.de>
2025-01-21 12:37:40 +0100

    lemmatization not done

---
+++ src/tokenization/lemmatization.jl
@@ -0,0 +1,11 @@
+"""
+lemmatize(input) lemmatizes regular words, based on some basic rules.
+only for english language
+"""
+
+function lemmatize(input::Vector{String})
+    #no idea how to do it with irregular words and so on,
+    #still a to do in general
+end
+export lemmatize
+

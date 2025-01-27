cf3823fa0474a55247da49449a780c7162346313
marike-v <marike_vossbeck@posteo.de>
2025-01-27 18:54:33 +0100

    Update make.jl to display chatgpt questions

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,8 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
-        "Getting Started" => "getting_started.md" 
+        "Getting Started" => "getting_started.md", 
+        "Llm Questions" => "llm_questions.md"
     ]
 )
 
